7633f5a831c91d2279036d53a7b3d3529cd0b7b1
ayushgupta0010 <ayushgupta0010@gmail.com>
2026-09-15 15:10:55 -0400

    Add TextSizeAdapter

--- app/src/main/java/edu/temple/inclassuiactivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiactivity/TextSizeAdapter.kt
@@ -1,25 +1,31 @@
 package edu.temple.inclassuiactivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+class TextSizeAdapter(private val ctx: Context, private val nums: Array<Int>) : BaseAdapter() {
 
-    override fun getCount(): Int {
-        TODO("Not yet implemented")
+    override fun getCount() = nums.size
+
+    override fun getItem(position: Int) = nums[position]
+
+    override fun getItemId(position: Int) = position.toLong()
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?) =
+        (if (convertView == null) TextView(ctx)
+        else {
+            (convertView as TextView)
+        }).apply {
+            text = nums[position].toString()
+            textSize = 22f
+        }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+        val textView = (getDropDownView(position, convertView, parent) as TextView)
+        textView.textSize = nums[position].toFloat()
+        return textView
     }
-
-    override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
-    }
-
-    override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
-    }
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
-    }
-
 }
